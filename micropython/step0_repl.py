84064b826eb81dab69f924be6f6b63d347a47582
Dan Picton <daniel.picton@gmail.com>
2018-04-20 20:38:40 +0000

    Add basic (and not yet functional) REPL

---
+++ micropython/step0_repl.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+def READ( input ):
+	return input
+
+def EVAL( input ):
+	return input
+
+def PRINT( input ):
+	return input
+
+def rep( input):
+	return (PRINT(EVAL(READ(input))))
+
+def main():
+	while True:
+		try:
+			line = input ("user> ")
+			rep(line)
+		except EOFError:
+			print("\nQuitting...")
+			break
+
+if __name__ == "__main__":
+    main()
